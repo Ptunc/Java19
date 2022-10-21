5136da04d7e87a24d78056d79a211e4353681245
Ptunc <spolentunc@gmail.com>
2022-10-21 11:38:18 +0300

    just commit

---
+++ java19.java
@@ -0,0 +1,13 @@
+package Java19;
+
+import java.util.Scanner;
+
+public class java19 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int a;
+        System.out.print("sayı girin: ");
+        a = input.nextInt();
+        for(int i=1; i<=a; i++) if(i%4 == 0 || i%5 == 0) System.out.println(i);
+    }
+}
